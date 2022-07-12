bfead7bf6e35567351b97b4c65b6aee602bebaa1
sajjad-feizollah <76600877+sajjad-feizollah@users.noreply.github.com>
2022-07-12 11:50:38 -0400

    Update cg_conc_func.m

--- general/cg_conc_func.m
+++ general/cg_conc_func.m
@@ -1,13 +1,29 @@
+% -_-_-_-_-_-_-_-_-_-_-_-_-_-_-cg_conc_func-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+%
+% Description: 
+% -----------
+% 
+% Calculates concomitant field basis functions 
+%
+% Inputs:
+% ------
+%
+%    x: meshgridded values of x- [Nx*Nx,1]
+%    y: meshgridded values of y- [Ny*Ny,1]
+%    z: meshgridded values of z- [Nz*Nz,1]
+%
+% Outputs:
+% -------
+% 
+%    H: concomitant gradient basis functions [4,N*N]
+%
+% Article: Feizollah and Tardif (2022)
+% -------
+%
+% Sajjad Feizollah, July 2022
+% -_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+
 function H=cg_conc_func(x,y,z)
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Generates spherical harmonics basis functions.
-%Input:
-%   x: meshgridded values of x- [Nx*Nx,1]
-%   y: meshgridded values of y- [Ny*Ny,1]
-%   z: meshgridded values of z- [Nz*Nz,1]
-% Output:
-%   H: concomitant gradient basis functions- [4,Nimg^2]
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 h(1,:,:) = z.*z;
 h(2,:,:) = x.*x + y.*y;
